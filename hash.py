c09f6f555ec3a76c781bd26e77e6666870aa595e
Marina F <fedma001@mail.ru>
2022-02-15 14:04:23 +0300

    hash done

--- hash.py
+++ hash.py
@@ -2,4 +2,12 @@ book = dict()
 book['apple'] = 0.67
 book['pear'] = 1.67
 book['bananas'] = 0.7
-print(book)
+#print(book['apple'])
+value = book.get('apple')
+def find_goods(text):
+    if book.get(text):
+        print ("Here is it!")
+    else:
+        book[text] = True
+        print ("OOPS")
+#find_goods('appl')
