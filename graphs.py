e88f9541757ecc963a15c91c4739184f04767a77
Marina F <fedma001@mail.ru>
2022-02-15 17:35:59 +0300

    praphs wide sick

--- graphs.py
+++ graphs.py
@@ -7,18 +7,21 @@ graph['anna'] = ['husband']
 #print(graph)
 from collections import deque
 from re import search
-search_que = deque()
-search_que += graph['me']
 def person_get_job(name):
-    return name[-1] == 'r'
-person_get_job('serg')
-def find_job():
+    return(name[-1]) == 'g'
+
+def search_wide(name):
+    search_que = deque()
+    search_que += graph[name]
+    searched = [] # для уже проверенных 
     while search_que:
         person = search_que.popleft()# из очереди извлекаем первого человека
-        if person_get_job:
-            print (person + 'He will get a jog')
-        return True
+        if not person in searched:
+            if person_get_job(person):
+                print (person + 'He will get a jog')
+                return True
     else:
         search_que += graph[person] # не найдет, берем второго
+        searched.append(person)
     return False
-print ({find_job})
+print(person_get_job('serg'))
