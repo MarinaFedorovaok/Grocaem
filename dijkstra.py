5aae31e93339cf859cb72158a2f932790542d92b
Marina F <fedma001@mail.ru>
2022-02-18 13:24:48 +0300

    printed dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,5 @@
+from concurrent.futures import process
+from json import load
 from tracemalloc import start
 
 
@@ -5,4 +7,55 @@ graph ={}
 graph[start] = {}
 graph[start]['a'] = 6
 graph[start]['b'] = 2
-print(graph)
+#print(graph[start].keys())
+graph['a'] = {}
+graph['a']['fin'] = 1
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+graph['fin'] = {}
+# print(graph)
+infinity = float('inf')# бесконечность 
+#веса
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = infinity
+#родители
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['in'] = None
+#массив для отбработанных узлов
+processed = []
+#подготовка завершена, алгоритм:
+    # пока остаются узлы
+    # взять узел, ближайший к началу
+    # обновить стоимости соседей
+    # если стоимости были обновлены, обновить и родителей
+    # пометить узел как обработанный
+def find_lowest_cost_node(costs):
+    lowest_cost = loaf('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost<lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neibours = graph[node]
+    for n in neibours.keys():
+        new_cost = cost+neibours[n]
+        if costs[n]>new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+print(node)
+
+
+
