2bebc027c83abcc5e7bcb9d4e8d9c43d4e1da6a6
Marina F <fedma001@mail.ru>
2022-02-18 19:10:14 +0300

    greedy works

--- greedy_algorithm.py
+++ greedy_algorithm.py
@@ -1,3 +1,5 @@
+import time
+
 states_needed = set(['ms', 'spb', 'lo', 'murm', 'vor', 'st', 'kr'])
 stations = {}
 stations['kone'] = set(['ms', 'lo', 'murm', 'st', 'kr' ])
@@ -6,19 +8,14 @@ stations['khtree'] = set(['st', 'kr'])
 stations['kfour'] = set(['ms', 'kr'])
 stations['kfive'] = set(['spb', 'lo', 'murm', 'kr'])
 final_stations = set()
-best_station = None
-states_covered = set()
 while states_needed:
+    best_station = None
+    states_covered = set()
     for station, states in stations.items():
         covered = states_needed & states
         if len(covered) > len(states_covered):
             best_station = station
-            print(best_station)
             states_covered = covered
-            print(states_covered)
-        states_needed -= states_covered
-        print(states_needed)
-        final_stations.add(best_station)
-        print(final_stations)
+    states_needed -= states_covered
+    final_stations.add(best_station)
 print (final_stations)
-
