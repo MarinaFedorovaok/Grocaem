e0edbb8d94962d3a6219ae69151d73c7210c8dce
MarinaFedorovaok <fedma001@mail.ru>
2022-03-19 15:45:46 +0300

    number 2 without print

--- ya.py
+++ ya.py
@@ -1,14 +1,14 @@
 n = int( input( 'n = ' ) )
 k = int( input( 'k = ' ) )
-m, rem = divmod( n, k )
-print (m)
-print (rem)
-res = []
-for i in range(rem):
-    res.append( m+1 )
-    print(res)
-for i in range( k-rem ):
-    res.append(m)
-    print(res)
+m, rest = divmod( n, k )
+#print (m)
+#print (rest)
+li = []
+for i in range(rest):
+    li.append( m+1 )
+    #print(li)
+for i in range( k-rest ):
+    li.append(m)
+    #print(li)
 print( f'Разбиение массива длиной {n} на {k} подмассивов с длинами:' )
-print(res)
+print(li)
